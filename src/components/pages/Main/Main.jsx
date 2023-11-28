bce05271856c08628b2c2f6d148f18877e5998ea
your-m1nd <vi.neurogirl@gmail.com>
2023-11-28 21:08:09 +0300

    add changes

--- src/components/pages/Main/Main.jsx
+++ src/components/pages/Main/Main.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Head from '../../views/Head/Head';
 import Foot from '../../views/Foot/Foot';
-import SimpleSlider from '../../comps/Slider/SimpleSlider';
 
 function Main() {
     return <React.Fragment>
         <Head></Head>
         <Foot></Foot>
-        <SimpleSlider/>
     </React.Fragment>
 }
 
